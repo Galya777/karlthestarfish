299d060625e2ed32f4a1ec3997ba60571348dc2f
Galya777 <galdodova@abv.bg>
2026-01-10 12:32:14 +0200

    license added

---
+++ composeApp/src/commonMain/kotlin/com/example/karlthestarfish/karl/KarlTypes.kt
@@ -0,0 +1,23 @@
+package com.example.karlthestarfish.karl
+
+enum class KarlMood {
+    PROUD,
+    CONFUSED,
+    INSPIRED,
+    DISTRACTED
+}
+enum class Intent {
+    TEACH,
+    TELL_STORY,
+    ANSWER_FACT,
+    ANSWER_MATH,
+    EVADE
+}
+
+enum class ReactionType {
+    CONFUSED,
+    ADVICE,
+    STORY,
+    JOKE,
+    RANDOM
+}
